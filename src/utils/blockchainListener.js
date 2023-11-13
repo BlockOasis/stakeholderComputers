ff185ac8ef659f18cac82b25d4c588e7f032ae65
Aryamanraj <20cs3011@rgipt.ac.in>
2023-11-14 04:16:36 +0530

    updated listener

--- src/utils/blockchainListener.js
+++ src/utils/blockchainListener.js
@@ -1,13 +1,16 @@
 const { Observable } = require('rxjs');
 const { getContract } = require('../smartcontract/utils/utils');
-const logger = require('./logger'); 
+const logger = require('./logger');
 
+/**
+ * Listens for the DataStored event and provides the IPFS CID to the observer.
+ */
 function getCID() {
     return new Observable(observer => {
         getContract().then(contract => {
-            contract.on("DataEvent", (sender, timestamp, ipfsCID) => {
-                logger.info(`Received CID: ${ipfsCID} from sender: ${sender} at timestamp: ${timestamp}`);
-                observer.next(ipfsCID); // Send the CID to the observer every time the event fires
+            contract.on("DataStored", (timestamp, cid) => {
+                logger.info(`Received CID: ${cid} at timestamp: ${timestamp}`);
+                observer.next(cid); // Send the CID to the observer every time the event fires
             });
         }).catch(error => {
             logger.error(`Error getting contract in getCID function: ${error}`);
@@ -15,11 +18,14 @@ function getCID() {
     });
 }
 
+/**
+ * Listens for the DataStored event and provides the timestamp to the observer.
+ */
 function getTimestampFromBlockchain() {
     return new Observable(observer => {
         getContract().then(contract => {
-            contract.on("DataEvent", (sender, timestamp, ipfsCID) => {
-                logger.info(`Received timestamp: ${timestamp} for CID: ${ipfsCID} from sender: ${sender}`);
+            contract.on("DataStored", (timestamp, cid) => {
+                logger.info(`Received timestamp: ${timestamp} for CID: ${cid}`);
                 observer.next(timestamp); // Send the timestamp to the observer every time the event fires
             });
         }).catch(error => {
